b7040e14c8a87595124549490f8e007576e5c52b
Ritika Fulwani <117587348+RitikaFulwani@users.noreply.github.com>
2024-06-07 17:11:55 +0530

    Add files via upload

---
+++ con1.java
@@ -0,0 +1,13 @@
+class test
+{
+  void test1()
+  {
+    System.out.println("Constructor Running");
+  }
+  public static void main(String args[])
+   {
+     test t = new test();
+	 t.test1();
+	 }
+}
+  
